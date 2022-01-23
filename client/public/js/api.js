67c6093df8913b8a823bc8df1b0d19371e92906c
amaury.delaroque <amaury.delaroque@gmail.com>
2022-01-23 22:08:05 +0100

    api url working

--- client/public/js/api.js
+++ client/public/js/api.js
@@ -1,5 +1,5 @@
 const api = {
-  baseUrl: "https://amaury-memory-back.herokuapp.com/api/v1/",
+  baseUrl: "https://amaury-memory-back.herokuapp.com/api/v1",
   storeScore: async (score, pseudo) => {
     try {
       const data = {
--- server/app/database.js
+++ server/app/database.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.PG_URL, {
   define: {
     timestamps: false,
   },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
